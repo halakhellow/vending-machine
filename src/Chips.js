d42acfbfe1440a85707d817c7eea7e047ea48745
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 22:08:06 +0300

    add chips image

--- src/Chips.js
+++ src/Chips.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import chips from "./images/chips.jfif";
 
 class Chips extends Component {
   render() {
     return (
       <div>
         <h1>Chips</h1>
+        <img src={chips} alt="chips" />
       </div>
     );
   }

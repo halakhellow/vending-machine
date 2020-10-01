813552dcbe8aee790579a5ac801093e20ad9c6c8
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 22:17:07 +0300

    Updates

---
+++ precache-manifest.0fcd0cd955521ca409667b899f23b884.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114af7afc12c5b14cdb5a11f4587fb08",
+    "url": "/vending-machine/index.html"
+  },
+  {
+    "revision": "c7fb472e84b85481f677",
+    "url": "/vending-machine/static/css/main.b984f75d.chunk.css"
+  },
+  {
+    "revision": "24048bdddb87ca053eb3",
+    "url": "/vending-machine/static/js/2.98271c03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vending-machine/static/js/2.98271c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7fb472e84b85481f677",
+    "url": "/vending-machine/static/js/main.5c7199b0.chunk.js"
+  },
+  {
+    "revision": "da6bf439b721347829a2",
+    "url": "/vending-machine/static/js/runtime-main.fb871aa5.js"
+  },
+  {
+    "revision": "af3ab921556554ac842089f46f1dc759",
+    "url": "/vending-machine/static/media/chips.af3ab921.jfif"
+  },
+  {
+    "revision": "416c7ae8310cfc315dc6a1f1d8c4f3bb",
+    "url": "/vending-machine/static/media/machine.416c7ae8.jpg"
+  },
+  {
+    "revision": "dfe366edef6b975388f402279ce90154",
+    "url": "/vending-machine/static/media/pepsi.dfe366ed.jpg"
+  },
+  {
+    "revision": "1219aa1fd1d95a8902bbac69dd860908",
+    "url": "/vending-machine/static/media/swiss-roll.1219aa1f.webp"
+  }
+]);

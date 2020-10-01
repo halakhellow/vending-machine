32a9c908bae69b0af632b98d1ed93345f8c3da99
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 21:15:41 +0300

    add Pepsi component

---
+++ src/Pepsi.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Pepsi extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pepsi</h1>
+      </div>
+    );
+  }
+}
+
+export default Pepsi;

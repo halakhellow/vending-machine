b12d9c397227c7e457e30143bc8075165694144f
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 22:08:40 +0300

    add swiss roll image

--- src/SwissRoll.js
+++ src/SwissRoll.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import swiss from "./images/swiss-roll.webp";
 
 class SwissRoll extends Component {
   render() {
     return (
       <div>
         <h1>SwissRoll</h1>
+        <img src={swiss} alt="swiss-roll" />
       </div>
     );
   }

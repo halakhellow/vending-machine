f58385f03473347abbef8d424fe4bd10add86cc0
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 21:15:49 +0300

    add SwissRoll component

---
+++ src/SwissRoll.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SwissRoll extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SwissRoll</h1>
+      </div>
+    );
+  }
+}
+
+export default SwissRoll;

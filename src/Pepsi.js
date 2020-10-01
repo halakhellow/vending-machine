c4a11c4dc7b272500fff23a9a692d86a3a268ef7
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 22:08:30 +0300

    add pepsi image

--- src/Pepsi.js
+++ src/Pepsi.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import pepsi from "./images/pepsi.jpg";
 
 class Pepsi extends Component {
   render() {
     return (
       <div>
         <h1>Pepsi</h1>
+        <img src={pepsi} alt="pepsi" />
       </div>
     );
   }

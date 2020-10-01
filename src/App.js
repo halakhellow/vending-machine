4c090fcb71c3428c9cca91f135f5e791301960e8
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 21:18:17 +0300

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import VendingMachine from "./VendingMachine";
+import Chips from "./Chips";
+import Pepsi from "./Pepsi";
+import SwissRoll from "./SwissRoll";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={VendingMachine} />
+      <Route exact path="/pepsi" component={Pepsi} />
+      <Route exact path="/chips" component={Chips} />
+      <Route exact path="/swiss-roll" component={SwissRoll} />
     </div>
   );
 }

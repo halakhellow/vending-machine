6a7196c36502b5c2faa5a6815b1039f9ac2f1dce
Hala Alkhellow <halakhellow1996@gmail.com>
2020-10-01 22:07:35 +0300

    style the NavBar component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 class NavBar extends Component {
   render() {
     return (
-      <div>
+      <div className="NavBar">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/pepsi">Pepsi</NavLink>
         <NavLink to="/chips">Chips</NavLink>
